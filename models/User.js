c33847e5226fe3d295ea287e3284dca5ce149a6d
Austin Wonka <wonkaaustin@gmail.com>
2022-03-11 14:21:22 -0500

    setup userschema and index file in the models folder

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            max_length: 20,
+        },
+        email: {
+            type: String,
+            match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\. [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                   'Please add a valid email address.',],
+            unique: true,
+            required: true,
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought'} ],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
