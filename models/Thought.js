d50804ac2e13720395976c293dd972cbb1024557
Austin Wonka <wonkaaustin@gmail.com>
2022-03-14 15:18:17 -0400

    had to put reactionschema before thoughtschema for initializing

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-            minlength: 1,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAt) => moment(createdAt).format('MMM Do YYYY')
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-    }
-);
-
 const reactionSchema = new Schema(
     {
         reactionId: {
@@ -51,6 +24,33 @@ const reactionSchema = new Schema(
     },
     {
         toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAt) => moment(createdAt).format('MMM Do YYYY')
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
             getters: true,
         },
     }
