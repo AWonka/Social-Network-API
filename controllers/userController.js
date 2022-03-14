8a55bb7ba616e797f60cf5d399688057113b050e
Austin Wonka <wonkaaustin@gmail.com>
2022-03-14 15:15:06 -0400

    changed findOneandremove to findoneanddelete for delete function

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,7 @@ module.exports = {
     },
     // delete a user
     deleteUser(req, res) {
-        User.findOneAndRemove({ _id: req.params.userId })
+        User.findOneAndDelete({ _id: req.params.userId })
             .then((user) =>
             !user
                 ? res.status(404).json({ message: 'No user found with that ID!' })
